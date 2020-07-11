36d2d4e8235c62246fa8579584714289cbead50a
ZhihaoMeng <zhmeng1992@163.com>
2020-07-10 23:25:23 -0400

    add RESTful API for posts

---
+++ api/views.py
@@ -0,0 +1,8 @@
+from Insta.models import Post
+from rest_framework import generics 
+from .serializers import PostSerializer
+# Create your views here.
+
+class PostAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer 
